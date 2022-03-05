df6a6e38c62fc9b66af7c4b61dd5cbe533c431b7
Kate Spitalnic <kespitalnic@gmail.com>
2022-03-05 00:10:40 -0500

    adding auth

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -65,7 +66,7 @@ router.get('/:id', (req, res) => {
       });
   });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
       title: req.body.title,
@@ -80,7 +81,7 @@ router.post('/', (req, res) => {
   });
 
   
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title
@@ -104,7 +105,7 @@ router.put('/:id', (req, res) => {
       });
   });
 
-  router.delete('/:id', (req, res) => {
+  router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
@@ -34,6 +33,15 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
   });
 
 module.exports = router;
