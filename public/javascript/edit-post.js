a4cf209d5b6d2cdba24771abbbfd742cd44ce848
Kate Spitalnic <kespitalnic@gmail.com>
2022-03-05 23:50:04 -0500

    add post content to edit post js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,16 +1,16 @@
-console.log('file loaded');
-
 async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_content = document.querySelector('input[name="post-content"]').value;
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+        title,
+        post_content
       }),
       headers: {
         'Content-Type': 'application/json'
